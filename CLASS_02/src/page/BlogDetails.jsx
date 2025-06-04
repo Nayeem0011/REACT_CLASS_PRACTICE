c2c28ffaea24fbc82d929574f590bd396d064005
Sk Nayeem <sknayeemislam384@.gmail.com>
2025-06-04 13:30:54 +0600

    ID_ADD

--- CLASS_02/src/page/BlogDetails.jsx
+++ CLASS_02/src/page/BlogDetails.jsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react'
+import { useParams } from 'react-router'
 
 const BlogDetails = () => {
+  const {id} = useParams()
   return (
     <Fragment>
-      <h1>This is blogderails</h1>
+      <h1>This is blogderails : {id}</h1>
     </Fragment>
   )
 }
